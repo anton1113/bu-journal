e731f8271e7b4b328ae4c8fb8e4bbfb395bdfc01
Anton Rasshchektaiev <anton.rasshchektaiev@auto1.com>
2023-02-27 10:54:09 +0200

    add group domain

--- src/test/java/com/arash/edu/bujournal/util/TestDataProvider.java
+++ src/test/java/com/arash/edu/bujournal/util/TestDataProvider.java
@@ -1,10 +1,12 @@
 package com.arash.edu.bujournal.util;
 
+import com.arash.edu.bujournal.domain.Group;
 import com.arash.edu.bujournal.domain.Student;
 import com.arash.edu.bujournal.domain.Teacher;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TestDataProvider {
@@ -13,15 +15,21 @@ public class TestDataProvider {
 
     public static Student createStudent() {
         Student student = new Student();
-        student.setFirstName(RandomStringUtils.randomAlphabetic(RANDOM_ALPHABETIC_LENGTH));
-        student.setLastName(RandomStringUtils.randomAlphabetic(RANDOM_ALPHABETIC_LENGTH));
+        student.setFirstName(randomAlphabetic(RANDOM_ALPHABETIC_LENGTH));
+        student.setLastName(randomAlphabetic(RANDOM_ALPHABETIC_LENGTH));
         return student;
     }
 
     public static Teacher createTeacher() {
         Teacher teacher = new Teacher();
-        teacher.setFirstName(RandomStringUtils.randomAlphabetic(RANDOM_ALPHABETIC_LENGTH));
-        teacher.setLastName(RandomStringUtils.randomAlphabetic(RANDOM_ALPHABETIC_LENGTH));
+        teacher.setFirstName(randomAlphabetic(RANDOM_ALPHABETIC_LENGTH));
+        teacher.setLastName(randomAlphabetic(RANDOM_ALPHABETIC_LENGTH));
         return teacher;
+    }
+
+    public static Group createGroup() {
+        Group group = new Group();
+        group.setName(randomAlphabetic(RANDOM_ALPHABETIC_LENGTH));
+        return group;
     }
 }
