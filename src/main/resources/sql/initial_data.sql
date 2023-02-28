dec4d0e607de71540b3a434f86f0d2f8f21710ab
Anton Rasshchektaiev <anton.rasshchektaiev@auto1.com>
2023-02-28 12:19:44 +0200

    insert initial data on startup

---
+++ src/main/resources/sql/initial_data.sql
@@ -0,0 +1,25 @@
+ALTER DATABASE bu CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- test teacher data
+insert into bu_teacher (id, first_name, last_name, patronymic)
+values (1, 'Ольга', 'Іванівна', 'Артеменко');
+
+-- group test data
+insert into bu_group (id, curator_id, name)
+values (1, 1, 'KM-501');
+
+-- student dest data
+insert into bu_student (id, first_name, group_id, last_name, patronymic)
+values (1, 'Антон', 1, 'Расщектаєв', 'Володимирович');
+
+-- discipline test data
+insert into bu_discipline (id, name, teacher_id)
+values (1, 'Основи програмування', 1);
+
+-- lesson test data
+insert into bu_lesson (id, date, discipline_id, group_id)
+values (1, '2023-03-01', 1, 1);
+
+-- attendance test data
+insert into bu_attendance (id, lesson_id, mark, student_id, was_present)
+values (1, 1, 5, 1, true);
