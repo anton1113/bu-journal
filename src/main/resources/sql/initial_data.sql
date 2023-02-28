7d7e31738c6378f46c911c0343977e322a632b6e
Anton Rasshchektaiev <anton.rasshchektaiev@auto1.com>
2023-02-28 16:29:59 +0200

    use fragment for footer

--- src/main/resources/sql/initial_data.sql
+++ src/main/resources/sql/initial_data.sql
@@ -12,12 +12,12 @@ values (1, 1, 'KM-501');
 insert into bu_student (id, first_name, group_id, last_name, patronymic)
 values (1, 'Антон', 1, 'Расщектаєв', 'Володимирович');
 
--- discipline test data
+-- subject test data
 insert into bu_subject (id, name, teacher_id, group_id)
 values (1, 'Основи програмування', 1 ,1);
 
 -- lesson test data
-insert into bu_lesson (id, date, discipline_id, group_id)
+insert into bu_lesson (id, date, subject_id, group_id)
 values (1, '2023-03-01', 1, 1);
 
 -- attendance test data
