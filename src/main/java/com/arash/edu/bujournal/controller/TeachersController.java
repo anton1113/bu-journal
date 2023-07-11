427cb3dd548dc63b100e422ef6608911331776dc
Anton Rasshchektaiev <anton.rasshchektaiev@auto1.com>
2023-07-11 16:48:15 +0300

    BU-18: implement edit teacher

--- src/main/java/com/arash/edu/bujournal/controller/TeachersController.java
+++ src/main/java/com/arash/edu/bujournal/controller/TeachersController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.UUID;
@@ -28,15 +29,28 @@ public class TeachersController {
         return "redirect:/teachers";
     }
 
+    @PostMapping("/teachers/{id}")
+    public String editTeacher(@PathVariable UUID id, @ModelAttribute Teacher teacher) {
+        teacherService.editTeacher(id, teacher);
+        return "redirect:/teachers";
+    }
+
     @GetMapping("/teachers/{id}/delete")
     public String deleteTeacher(@PathVariable UUID id) {
         teacherService.deleteTeacher(id);
         return "redirect:/teachers";
     }
 
+    @GetMapping("/teachers/{id}/draft")
+    public String getTeacherDraft(@PathVariable UUID id, RedirectAttributes redirectAttrs) {
+        Teacher teacher = teacherService.findById(id);
+        redirectAttrs.addFlashAttribute("editTeacherDraft", teacher);
+        return "redirect:/teachers";
+    }
+
     private void populateAllTeachers(Model model) {
         List<Teacher> teachers = teacherService.findAll();
         model.addAttribute("teachers", teachers);
-        model.addAttribute("draftTeacher", new Teacher());
+        model.addAttribute("addTeacherDraft", new Teacher());
     }
 }
