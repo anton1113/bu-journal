537cdb9a35ed97efffb3f2a76067c6506b1427e4
Anton Rasshchektaiev <anton11131113@gmail.com>
2023-08-09 18:03:33 +0300

    BU-48_student-ux (#49)
    * BU-48: add anonymous UX
    
    * BU-48: separate /me and /index pages
    
    * BU-48: apply page-container class

--- src/main/java/com/arash/edu/bujournal/controller/FeedbackController.java
+++ src/main/java/com/arash/edu/bujournal/controller/FeedbackController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class FeedbackController {
 
-    @GetMapping(value = {"/", "/index"})
-    public String showIndexPage() {
-        return "index";
+    @GetMapping(value = {"/feedback"})
+    public String showFeedbackPage() {
+        return "feedback";
     }
 }
